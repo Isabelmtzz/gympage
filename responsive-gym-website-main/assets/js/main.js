d224dc066ee9009820a8425b9a760aa5191d5857
Isabelmtzz <reynaisabmtzz@gmail.com>
2023-07-29 01:54:43 -0600

    second commit

--- responsive-gym-website-main/assets/js/main.js
+++ responsive-gym-website-main/assets/js/main.js
@@ -1,12 +1,47 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")
+
+
+    /*=============== MENU SHOW===============*/
+if(navToggle){
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
+}
+    /*=============== MENU SHOW===============*/
+
+if(navClose){
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll(".nav__link")
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu")
+    navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach(e => e.addEventListener("click", linkAction))
+
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+navLink.forEach(e => e.addEventListener("click", linkAction))
 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById("header")
+    this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header")
+}
 
+window.addEventListener("scroll", scrollHeader)
+  
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
